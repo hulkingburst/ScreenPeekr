e5d466a9241215e2411e7bf16333fe7818e4fc2d
hulkingburst <renowhn@gmail.com>
2026-06-22 00:15:19 -0400

    added updates and fixed various bugs

--- Tray/WebhookInstructionsWindow.cs
+++ Tray/WebhookInstructionsWindow.cs
@@ -11,47 +11,53 @@ internal sealed class WebhookInstructionsWindow : Form
         MaximizeBox = false;
         ClientSize = new Size(600, 450);
 
-        var instructions = new TextBox
+        var instructions = new RichTextBox
         {
-            Text = @"Setting Up a Discord Webhook for ScreenPeekr
-
-1. Create a Private Discord Server
-   - Open Discord.
-   - Click the + button on the left sidebar.
-   - Click Create My Own.
-   - Click For me and my friends.
-   - Enter a server name and click Create.
-
-2. Create a Webhook
-   - Open the channel where you want screenshots to be sent (such as #general).
-   - Click the gear icon next to the channel.
-   - Click Integrations.
-   - Click Webhooks.
-   - Click Create Webhook.
-   - Give the webhook a name (for example, ScreenPeekr).
-   - Click Save Changes.
-
-3. Copy the Webhook URL
-   - Click on the webhook you just created.
-   - Click Copy Webhook URL.
-
-4. Add the Webhook to ScreenPeekr
-   - Open ScreenPeekr.
-   - Go to Settings.
-   - Find the Webhook URL setting.
-   - Paste the copied Discord webhook URL into the field.
-   - Save your settings.
-
-ScreenPeekr will now send screenshots directly to your Discord channel whenever screenshot uploading is enabled. Keep your webhook URL private, as anyone with the URL can send messages to the channel.",
             Left = 12,
             Top = 12,
             Width = 576,
             Height = 380,
-            Multiline = true,
             ReadOnly = true,
-            ScrollBars = ScrollBars.Vertical,
-            Font = new System.Drawing.Font("Segoe UI", 10F)
+            ScrollBars = RichTextBoxScrollBars.Vertical,
+            Font = new System.Drawing.Font("Segoe UI", 10F),
+            BackColor = System.Drawing.Color.White,
+            BorderStyle = BorderStyle.FixedSingle
         };
+
+        // RTF formatted text with rich formatting
+        instructions.Rtf = @"{\rtf1\ansi\ansicpg1252\deff0\deflang1033{\fonttbl{\f0\fnil\fcharset0 Segoe UI;}}
+{\colortbl ;\red0\green0\blue255;\red255\green0\blue0;}
+\viewkind4\uc1\pard\cf1\b\fs24 Setting Up a Discord Webhook for ScreenPeekr\par
+\par
+\cf0\b0\fs20 \b 1. Create a Private Discord Server\b0\par
+\pard\li360\bullet - Open Discord.\par
+- Click the \b +\b0 button on the left sidebar.\par
+- Click \b Create My Own\b0.\par
+- Click \b For me and my friends\b0.\par
+- Enter a server name and click \b Create\b0.\par
+\pard\par
+\b 2. Create a Webhook\b0\par
+\pard\li360\bullet - Open the channel where you want screenshots to be sent (such as \b #general\b0).\par
+- Click the gear icon next to the channel.\par
+- Click \b Integrations\b0.\par
+- Click \b Webhooks\b0.\par
+- Click \b Create Webhook\b0.\par
+- Give the webhook a name (for example, \b ScreenPeekr\b0).\par
+- Click \b Save Changes\b0.\par
+\pard\par
+\b 3. Copy the Webhook URL\b0\par
+\pard\li360\bullet - Click on the webhook you just created.\par
+- Click \b Copy Webhook URL\b0.\par
+\pard\par
+\b 4. Add the Webhook to ScreenPeekr\b0\par
+\pard\li360\bullet - Open \b ScreenPeekr\b0.\par
+- Go to \b Settings\b0.\par
+- Find the \b Webhook URL\b0 setting.\par
+- Paste the copied Discord webhook URL into the field.\par
+- Save your settings.\par
+\pard\par
+\pard\li0\i ScreenPeekr will now send screenshots directly to your Discord channel whenever screenshot uploading is enabled.\i0  \cf2\b Keep your webhook URL private\b0\cf0, as anyone with the URL can send messages to the channel.\par
+}";
 
         var closeButton = new Button
         {
--- install.bat
+++ install.bat
@@ -1,26 +1,31 @@
 @echo off
 echo Installing ScreenPeekr...
 
-REM Check if running as administrator
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo Please run this installer as Administrator.
-    echo Right-click install.bat and select "Run as administrator"
-    pause
-    exit /b 1
-)
-
-REM Set installation directory
-set "INSTALL_DIR=C:\Program Files\ScreenPeekr"
+REM Set installation directory (user profile to avoid admin requirement)
+set "INSTALL_DIR=%LOCALAPPDATA%\ScreenPeekr"
 
 REM Create installation directory
 if not exist "%INSTALL_DIR%" (
     mkdir "%INSTALL_DIR%"
 )
 
+REM Determine build configuration
+if exist "bin\Debug\net8.0-windows\ScreenPeekr.exe" (
+    set "SOURCE_DIR=bin\Debug\net8.0-windows"
+) else if exist "bin\Release\net8.0-windows\ScreenPeekr.exe" (
+    set "SOURCE_DIR=bin\Release\net8.0-windows"
+) else if exist "bin\Release\publish\ScreenPeekr.exe" (
+    set "SOURCE_DIR=bin\Release\publish"
+) else (
+    echo Error: Could not find built executable.
+    echo Please build the project first using: dotnet build -c Release
+    pause
+    exit /b 1
+)
+
 REM Copy files
-echo Copying files to %INSTALL_DIR%...
-copy /Y "bin\Release\publish\ScreenPeekr.exe" "%INSTALL_DIR%\"
+echo Copying files from %SOURCE_DIR% to %INSTALL_DIR%...
+xcopy /Y /E /I "%SOURCE_DIR%\*" "%INSTALL_DIR%\"
 if %errorLevel% neq 0 (
     echo Failed to copy files.
     pause
@@ -29,12 +34,12 @@ if %errorLevel% neq 0 (
 
 REM Create desktop shortcut
 echo Creating desktop shortcut...
-powershell -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%USERPROFILE%\Desktop\ScreenPeekr.lnk'); $Shortcut.TargetPath = '%INSTALL_DIR%\ScreenPeekr.exe'; $Shortcut.Save()"
+powershell -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%USERPROFILE%\Desktop\ScreenPeekr.lnk'); $Shortcut.TargetPath = '%INSTALL_DIR%\ScreenPeekr.exe'; $Shortcut.WorkingDirectory = '%INSTALL_DIR%'; $Shortcut.Save()"
 
 REM Ask if user wants to start with Windows
 set /p STARTUP="Do you want ScreenPeekr to start with Windows? (Y/N): "
 if /i "%STARTUP%"=="Y" (
-    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "ScreenPeekr" /t REG_SZ /d "%INSTALL_DIR%\ScreenPeekr.exe" /f
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "ScreenPeekr" /t REG_SZ /d "\"%INSTALL_DIR%\ScreenPeekr.exe\"" /f
     echo ScreenPeekr will start with Windows.
 )
 
